894cf83144a76bd4c9f2f88fff3b6ce76fbca679
krlvm <krlvm@users.noreply.github.com>
2019-10-28 10:48:57 +0300

    Make it more useful

--- src/ru/krlvm/powertunnel/utilities/PacketUtility.java
+++ src/ru/krlvm/powertunnel/utilities/PacketUtility.java
@@ -21,7 +21,18 @@ public class PacketUtility {
      * @return - ByteBuf chunks
      */
     public static LinkedList<ByteBuf> bufferChunk(ByteBuf buf) {
-        LinkedList<byte[]> chunks = chunk(buf);
+        return bufferChunk(buf, 1);
+    }
+
+    /**
+     * Retrieves list of packet's ByteBuf chunks
+     *
+     * @param buf - ByteBuf of packet
+     * @param chunkSize - size of chunk
+     * @return - ByteBuf chunks
+     */
+    public static LinkedList<ByteBuf> bufferChunk(ByteBuf buf, int chunkSize) {
+        LinkedList<byte[]> chunks = chunk(buf, chunkSize);
         LinkedList<ByteBuf> buffers = new LinkedList<>();
         for (byte[] chunk : chunks) {
             buffers.add(Unpooled.wrappedBuffer(chunk));
@@ -36,11 +47,21 @@ public class PacketUtility {
      * @return - ByteBuf chunks (byte[])
      */
     public static LinkedList<byte[]> chunk(ByteBuf buf) {
+        return chunk(buf, 1);
+    }
+
+    /**
+     * Retrieves list (byte[]) of packet's ByteBuf chunks
+     *
+     * @param buf - ByteBuf of packet
+     * @param chunkSize - size of chunk
+     * @return - ByteBuf chunks (byte[])
+     */
+    public static LinkedList<byte[]> chunk(ByteBuf buf, int chunkSize) {
         byte[] bytes = new byte[buf.readableBytes()];
         buf.readBytes(bytes);
         LinkedList<byte[]> byteChunks = new LinkedList<>();
         int len = bytes.length;
-        int chunkSize = 1;
         int i = 0;
         while (i < len) {
             byteChunks.add(Arrays.copyOfRange(bytes, i, i += chunkSize));
