d581efc985edef883d9eabba2db3013edd3a59cb
krlvm <krlvm@users.noreply.github.com>
2019-11-02 12:57:18 +0300

    Version 1.3.1 bump

--- src/ru/krlvm/powertunnel/PowerTunnel.java
+++ src/ru/krlvm/powertunnel/PowerTunnel.java
@@ -38,7 +38,7 @@ import java.util.*;
 public class PowerTunnel {
 
     public static final String NAME = "PowerTunnel";
-    public static final String VERSION = "1.3";
+    public static final String VERSION = "1.3.1";
     public static final int VERSION_CODE = 3;
     public static final String REPOSITORY_URL = "https://github.com/krlvm/PowerTunnel";
 
