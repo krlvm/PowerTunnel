d3656c8132569c417b1443966767486b8d43ed09
krlvm <krlvm@users.noreply.github.com>
2020-01-01 15:40:24 +0300

    Bump version to 1.7.1

--- src/ru/krlvm/powertunnel/PowerTunnel.java
+++ src/ru/krlvm/powertunnel/PowerTunnel.java
@@ -40,8 +40,8 @@ import java.util.*;
 public class PowerTunnel {
 
     public static final String NAME = "PowerTunnel";
-    public static final String VERSION = "1.7";
-    public static final int VERSION_CODE = 8;
+    public static final String VERSION = "1.7.1";
+    public static final int VERSION_CODE = 9;
     public static final String REPOSITORY_URL = "https://github.com/krlvm/PowerTunnel";
 
     private static HttpProxyServer SERVER;
