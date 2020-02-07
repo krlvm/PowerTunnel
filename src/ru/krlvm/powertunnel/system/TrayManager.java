bc14ad3a32809dd53d3d3bd4f1b8cfca03b65e63
krlvm <51774833+krlvm@users.noreply.github.com>
2020-02-07 16:23:07 +0300

    Update tray popup

--- src/ru/krlvm/powertunnel/system/TrayManager.java
+++ src/ru/krlvm/powertunnel/system/TrayManager.java
@@ -2,11 +2,14 @@ package ru.krlvm.powertunnel.system;
 
 import ru.krlvm.powertunnel.PowerTunnel;
 import ru.krlvm.powertunnel.utilities.UIUtility;
+import ru.krlvm.swingdpi.SwingDPI;
 
-import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.font.TextAttribute;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TrayManager {
 
@@ -14,17 +17,93 @@ public class TrayManager {
 
     public void load() throws AWTException {
         PopupMenu popup = new PopupMenu();
+
+        Map<TextAttribute, Object> attributes = new HashMap<>();
+        attributes.put(TextAttribute.SIZE, 12*SwingDPI.getScaleFactor());
+        Font font = Font.getFont(attributes);
+        attributes.put(TextAttribute.FAMILY, Font.DIALOG);
+        attributes.put(TextAttribute.WEIGHT, TextAttribute.WEIGHT_BOLD);
+        Font headerFont = Font.getFont(attributes);
+
         MenuItem item;
 
-        item = new MenuItem(PowerTunnel.NAME + " is" + PowerTunnel.getStatus().toString());
+        item = new MenuItem(PowerTunnel.NAME + " " + PowerTunnel.VERSION);
+        item.setFont(headerFont);
         item.setEnabled(false);
         popup.add(item);
 
         item = new MenuItem("(c) krlvm, 2019-2020");
+        item.setFont(headerFont);
+        item.setEnabled(false);
+        popup.add(item);
+
+        item = new MenuItem("");
+        item.setFont(font);
+        item.setEnabled(false);
+        popup.add(item);
+
+        item = new MenuItem("Open PowerTunnel");
+        item.setFont(font);
+        item.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                PowerTunnel.showMainFrame();
+            }
+        });
+        popup.add(item);
+
+        item = new MenuItem("");
+        item.setFont(font);
+        item.setEnabled(false);
+        popup.add(item);
+
+        item = new MenuItem("Logs");
+        item.setFont(font);
+        item.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                PowerTunnel.logFrame.setVisible(true);
+            }
+        });
+        popup.add(item);
+
+        item = new MenuItem("Journal");
+        item.setFont(font);
+        item.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                PowerTunnel.journalFrame.setVisible(true);
+            }
+        });
+        popup.add(item);
+
+        item = new MenuItem("Blacklist");
+        item.setFont(font);
+        item.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                PowerTunnel.USER_FRAMES[0].setVisible(true);
+            }
+        });
+        popup.add(item);
+
+        item = new MenuItem("Whitelist");
+        item.setFont(font);
+        item.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                PowerTunnel.USER_FRAMES[1].setVisible(true);
+            }
+        });
+        popup.add(item);
+
+        item = new MenuItem("");
+        item.setFont(font);
         item.setEnabled(false);
         popup.add(item);
 
         item = new MenuItem("Exit");
+        item.setFont(headerFont);
         item.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
