69e2b4244fcf103ad73cde9be21592b104679b18
krlvm <51774833+krlvm@users.noreply.github.com>
2021-08-14 01:35:42 +0400

    Core: remove 'v' from BuildConstants.VERSION

--- core/src/main/java/io/github/krlvm/powertunnel/BuildConstants.java
+++ core/src/main/java/io/github/krlvm/powertunnel/BuildConstants.java
@@ -20,7 +20,7 @@ package io.github.krlvm.powertunnel;
 public class BuildConstants {
     public static final String NAME = "PowerTunnel";
     public static final String REPO = "https://github.com/krlvm/PowerTunnel/tree/next";
-    public static final String VERSION = "v2.0-alpha";
+    public static final String VERSION = "2.0-alpha";
     public static final int VERSION_CODE = 90;
 
     public static boolean IS_RELEASE = isReleaseVersion(VERSION);
