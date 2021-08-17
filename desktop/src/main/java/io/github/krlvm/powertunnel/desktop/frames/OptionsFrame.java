ae784c6e1fbbd860543a204ee918958c2a6b4a47
krlvm <51774833+krlvm@users.noreply.github.com>
2021-08-17 21:59:45 +0400

    Desktop: fixed an issue when locale was not applied to Proxy Settings group header

--- desktop/src/main/java/io/github/krlvm/powertunnel/desktop/frames/OptionsFrame.java
+++ desktop/src/main/java/io/github/krlvm/powertunnel/desktop/frames/OptionsFrame.java
@@ -107,7 +107,7 @@ public class OptionsFrame extends PreferencesFrame {
                     PreferenceType.SWITCH
             ));
 
-            groups.add(new PreferenceGroup("Proxy settings", null, preferences));
+            groups.add(new PreferenceGroup(I18N.get("options.group.proxySettings"), null, preferences));
         }
 
         return groups;
