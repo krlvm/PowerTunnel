2e30eaacda77f4ec5037f48841dd57911b492343
krlvm <51774833+krlvm@users.noreply.github.com>
2021-08-18 21:27:39 +0400

    Desktop: fixed an issue when preferences frame was not disposed if user declined to restart server

--- desktop/src/main/java/io/github/krlvm/powertunnel/desktop/frames/PreferencesFrame.java
+++ desktop/src/main/java/io/github/krlvm/powertunnel/desktop/frames/PreferencesFrame.java
@@ -346,6 +346,8 @@ public class PreferencesFrame extends AppFrame {
                     GraphicalApp.getInstance().stop();
                     GraphicalApp.getInstance().start();
                 });
+            } else {
+                dispose();
             }
         }
     }
