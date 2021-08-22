a85055749863dba2dd6ec722ff4027c1e990ed2f
krlvm <51774833+krlvm@users.noreply.github.com>
2021-08-22 22:52:25 +0400

    Core: migrate from org.json to gson to support Android
    Android have built-in org.json which is incompatible with app's one

--- core/src/main/java/io/github/krlvm/powertunnel/preferences/PreferenceParser.java
+++ core/src/main/java/io/github/krlvm/powertunnel/preferences/PreferenceParser.java
@@ -17,11 +17,9 @@
 
 package io.github.krlvm.powertunnel.preferences;
 
+import com.google.gson.*;
 import io.github.krlvm.powertunnel.exceptions.PreferenceParseException;
 import io.github.krlvm.powertunnel.i18n.I18NBundle;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.util.*;
 
@@ -30,36 +28,35 @@ public class PreferenceParser {
     public static final String FILE = "preferences.json";
 
     public static List<PreferenceGroup> parsePreferences(String source, String json, I18NBundle bundle) throws PreferenceParseException {
-        final JSONArray array;
+        final JsonArray array;
         try {
-            array = new JSONArray(json);
-        } catch (JSONException ex) {
+            array = JsonParser.parseString(json).getAsJsonArray();
+        } catch (IllegalStateException | JsonSyntaxException ex) {
             throw new PreferenceParseException(source, "Malformed preferences JSON", ex);
         }
 
         final List<PreferenceGroup> groups = new ArrayList<>();
         final List<Preference> ungrouped = new ArrayList<>();
-        for (Object object : array) {
-            if (!(object instanceof JSONObject))
-                throw new PreferenceParseException(source, "Malformed preferences structure");
-            final JSONObject jso = ((JSONObject) object);
+        for (JsonElement element : array) {
+            if (!element.isJsonObject()) throw new PreferenceParseException(source, "Malformed preferences structure");
+            final JsonObject jso = element.getAsJsonObject();
             if(jso.has(PreferencesGroupSchemaFields.ID)) {
                 if (!jso.has(PreferencesGroupSchemaFields.PREFERENCES))
                     throw new PreferenceParseException(
                             source,
                             "One of preference groups is incomplete (missing 'preferences')"
                     );
-                final Object jsoList = jso.get(PreferencesGroupSchemaFields.PREFERENCES);
-                if(!(jsoList instanceof JSONArray)) throw new PreferenceParseException(source, "Preferences list should be array");
+                final JsonElement jsoList = jso.get(PreferencesGroupSchemaFields.PREFERENCES);
+                if(!jsoList.isJsonArray()) throw new PreferenceParseException(source, "Preferences list should be array");
 
-                final String title = jso.has(PreferencesGroupSchemaFields.TITLE) ? jso.getString(PreferencesGroupSchemaFields.TITLE) :
-                        bundle.get(jso.getString(PreferencesGroupSchemaFields.ID));
-                final String description = jso.has(PreferencesGroupSchemaFields.DESCRIPTION) ? jso.getString(PreferencesGroupSchemaFields.DESCRIPTION) :
-                        bundle.get(jso.getString(PreferencesGroupSchemaFields.ID) + ".desc", null);
+                final String title = jso.has(PreferencesGroupSchemaFields.TITLE) ? jso.get(PreferencesGroupSchemaFields.TITLE).getAsString() :
+                        bundle.get(jso.get(PreferencesGroupSchemaFields.ID).getAsString());
+                final String description = jso.has(PreferencesGroupSchemaFields.DESCRIPTION) ? jso.get(PreferencesGroupSchemaFields.DESCRIPTION).getAsString() :
+                        bundle.get(jso.get(PreferencesGroupSchemaFields.ID).getAsString() + ".desc", null);
                 groups.add(new PreferenceGroup(
                         title,
                         description,
-                        parsePreferenceList(source, ((JSONArray) jsoList), bundle)
+                        parsePreferenceList(source, jsoList.getAsJsonArray(), bundle)
                 ));
             } else {
                 ungrouped.add(parsePreference(source, jso, bundle));
@@ -71,28 +68,28 @@ public class PreferenceParser {
         return groups;
     }
 
-    public static List<Preference> parsePreferenceList(String source, JSONArray array, I18NBundle bundle) throws PreferenceParseException {
+    public static List<Preference> parsePreferenceList(String source, JsonArray array, I18NBundle bundle) throws PreferenceParseException {
         final List<Preference> preferences = new ArrayList<>();
-        for (Object object : array) {
-            if (!(object instanceof JSONObject))
+        for (JsonElement object : array) {
+            if (!object.isJsonObject())
                 throw new PreferenceParseException(source, "Malformed preferences list structure");
-            preferences.add(parsePreference(source, ((JSONObject) object), bundle));
+            preferences.add(parsePreference(source, object.getAsJsonObject(), bundle));
         }
         return preferences;
     }
 
-    public static Preference parsePreference(String source, JSONObject jso, I18NBundle bundle) throws PreferenceParseException {
+    public static Preference parsePreference(String source, JsonObject jso, I18NBundle bundle) throws PreferenceParseException {
         if (!jso.has(PreferencesSchemaFields.KEY) || !jso.has(PreferencesSchemaFields.TYPE))
             throw new PreferenceParseException(source, "One of preferences is incomplete (missing 'key' and (or) 'type')");
 
-        final String rawType = jso.getString(PreferencesSchemaFields.TYPE);
+        final String rawType = jso.get(PreferencesSchemaFields.TYPE).getAsString();
         final PreferenceType type;
         try {
             type = PreferenceType.valueOf(rawType.toUpperCase());
         } catch (IllegalArgumentException ex) {
             throw new PreferenceParseException(source, "Unsupported preference type: '" + rawType + '"', ex);
         }
-        final String key = jso.getString(PreferencesSchemaFields.KEY);
+        final String key = jso.get(PreferencesSchemaFields.KEY).getAsString();
 
         Map<String, String> items = null;
         if(type == PreferenceType.SELECT) {
@@ -101,48 +98,48 @@ public class PreferenceParser {
             }
             items = new LinkedHashMap<>();
 
-            final Object jsoItemsList = jso.get(PreferencesSchemaFields.ITEMS);
-            if(!(jsoItemsList instanceof JSONArray)) throw new PreferenceParseException(source, "Select Preference item list should be array");
-            final JSONArray itemsArray = ((JSONArray) jsoItemsList);
-            for (Object o : itemsArray) {
-                if(!(o instanceof JSONObject))
+            final JsonElement jsoItemsList = jso.get(PreferencesSchemaFields.ITEMS);
+            if(!jsoItemsList.isJsonArray()) throw new PreferenceParseException(source, "Select Preference item list should be array");
+            final JsonArray itemsArray = jsoItemsList.getAsJsonArray();
+            for (JsonElement o : itemsArray) {
+                if(!o.isJsonObject())
                     throw new PreferenceParseException(source, "Malformed select preference items structure");
-                final JSONObject ijo = ((JSONObject) o);
+                final JsonObject ijo = o.getAsJsonObject();
                 if(!ijo.has(PreferencesSelectItemSchemaFields.KEY))
                     throw new PreferenceParseException(source, "One of select preferences items is incomplete (missing 'key')");
 
-                final String ikey = ijo.getString(PreferencesSelectItemSchemaFields.KEY);
-                final String name = ijo.has(PreferencesSelectItemSchemaFields.NAME) ? ijo.getString(PreferencesSelectItemSchemaFields.NAME) :
+                final String ikey = ijo.get(PreferencesSelectItemSchemaFields.KEY).getAsString();
+                final String name = ijo.has(PreferencesSelectItemSchemaFields.NAME) ? ijo.get(PreferencesSelectItemSchemaFields.NAME).getAsString() :
                         bundle.get(key + ".item." + ikey);
                 items.put(ikey, name);
             }
         }
 
-        final Object defaultValue = getObjectOrNull(jso, PreferencesSchemaFields.DEFAULT_VALUE);
-        final Object dependencyValue = getObjectOrNull(jso, PreferencesSchemaFields.DEPENDENCY_VALUE);
+        final JsonElement defaultValue = getElementOrNull(jso, PreferencesSchemaFields.DEFAULT_VALUE);
+        final JsonElement dependencyValue = getElementOrNull(jso, PreferencesSchemaFields.DEPENDENCY_VALUE);
 
-        final String title = jso.has(PreferencesSchemaFields.TITLE) ? jso.getString(PreferencesSchemaFields.TITLE) :
-                bundle.get(jso.getString(PreferencesSchemaFields.KEY));
-        final String description = jso.has(PreferencesSchemaFields.DESCRIPTION) ? jso.getString(PreferencesSchemaFields.DESCRIPTION) :
-                bundle.get(jso.getString(PreferencesSchemaFields.KEY) + ".desc", null);
+        final String title = jso.has(PreferencesSchemaFields.TITLE) ? jso.get(PreferencesSchemaFields.TITLE).getAsString() :
+                bundle.get(jso.get(PreferencesSchemaFields.KEY).getAsString());
+        final String description = jso.has(PreferencesSchemaFields.DESCRIPTION) ? jso.get(PreferencesSchemaFields.DESCRIPTION).getAsString() :
+                bundle.get(jso.get(PreferencesSchemaFields.KEY).getAsString() + ".desc", null);
 
         return new Preference(
                 key,
                 title,
                 description,
-                defaultValue != null ? defaultValue.toString() : type.getDefaultValue(),
+                defaultValue != null ? defaultValue.getAsString() : type.getDefaultValue(),
                 type,
                 getStringOrNull(jso, PreferencesSchemaFields.DEPENDENCY),
-                dependencyValue != null ? dependencyValue.toString() : "true",
+                dependencyValue != null ? dependencyValue.getAsString() : "true",
                 items
         );
     }
 
-    private static String getStringOrNull(JSONObject jso, String key) {
-        return jso.has(key) ? jso.getString(key) : null;
+    private static String getStringOrNull(JsonObject jso, String key) {
+        return jso.has(key) ? jso.get(key).getAsString() : null;
     }
 
-    private static Object getObjectOrNull(JSONObject jso, String key) {
+    private static JsonElement getElementOrNull(JsonObject jso, String key) {
         return jso.has(key) ? jso.get(key) : null;
     }
 
