90b37f2f71fbfb65a2196d44dd19ce3164cf387a
krlvm <51774833+krlvm@users.noreply.github.com>
2021-08-24 12:38:36 +0400

    Bump version to 2.0-rc3 (97)

--- desktop/src/main/java/io/github/krlvm/powertunnel/desktop/BuildConstants.java
+++ desktop/src/main/java/io/github/krlvm/powertunnel/desktop/BuildConstants.java
@@ -21,8 +21,8 @@ public class BuildConstants {
     public static final String NAME = "PowerTunnel";
     public static final String DESCRIPTION = "Powerful and extensible proxy server";
     public static final String REPO = "https://github.com/krlvm/PowerTunnel/tree/next";
-    public static final String VERSION = "2.0-rc2";
-    public static final int VERSION_CODE = 96;
+    public static final String VERSION = "2.0-rc3";
+    public static final int VERSION_CODE = 97;
 
     public static boolean IS_RELEASE = io.github.krlvm.powertunnel.BuildConstants.isReleaseVersion(VERSION);
     public static final boolean DEBUG = !IS_RELEASE;
