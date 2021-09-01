257fc18c004e9672b5f1eaf2735e657826a93ed4
krlvm <51774833+krlvm@users.noreply.github.com>
2021-09-01 13:12:46 +0300

    Core: Bump version to 2.0-rc4u1

--- core/src/main/java/io/github/krlvm/powertunnel/BuildConstants.java
+++ core/src/main/java/io/github/krlvm/powertunnel/BuildConstants.java
@@ -20,7 +20,7 @@ package io.github.krlvm.powertunnel;
 public class BuildConstants {
     public static final String NAME = "PowerTunnel";
     public static final String REPO = "https://github.com/krlvm/PowerTunnel/tree/next";
-    public static final String VERSION = "2.0-rc4";
+    public static final String VERSION = "2.0-rc4u1";
     public static final int VERSION_CODE = 98;
 
     public static boolean IS_RELEASE = isReleaseVersion(VERSION);
