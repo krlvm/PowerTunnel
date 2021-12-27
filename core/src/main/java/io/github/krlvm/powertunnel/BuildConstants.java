b5a1e5088c80deb574c66e99efcfd5d0fb8807b5
krlvm <51774833+krlvm@users.noreply.github.com>
2021-12-27 21:39:47 +0300

    Fix repository link

--- core/src/main/java/io/github/krlvm/powertunnel/BuildConstants.java
+++ core/src/main/java/io/github/krlvm/powertunnel/BuildConstants.java
@@ -19,7 +19,7 @@ package io.github.krlvm.powertunnel;
 
 public class BuildConstants {
     public static final String NAME = "PowerTunnel";
-    public static final String REPO = "https://github.com/krlvm/PowerTunnel/";
+    public static final String REPO = "https://github.com/krlvm/PowerTunnel";
     public static final String VERSION = "2.0";
     public static final int VERSION_CODE = 100;
     public static final int SDK = 100;
--- desktop/src/main/java/io/github/krlvm/powertunnel/desktop/BuildConstants.java
+++ desktop/src/main/java/io/github/krlvm/powertunnel/desktop/BuildConstants.java
@@ -20,7 +20,7 @@ package io.github.krlvm.powertunnel.desktop;
 public class BuildConstants {
     public static final String NAME = "PowerTunnel";
     public static final String DESCRIPTION = "Powerful and extensible proxy server";
-    public static final String REPO = "https://github.com/krlvm/PowerTunnel/tree/next";
+    public static final String REPO = "https://github.com/krlvm/PowerTunnel";
     public static final String VERSION = "2.0";
     public static final int VERSION_CODE = 100;
 
