9f8e3b4e438213b0f610d39fd6619b5c6689a5e2
krlvm <51774833+krlvm@users.noreply.github.com>
2022-02-08 23:20:28 +0300

    Core: Bump version to 2.2 (103), SDK 103

--- core/src/main/java/io/github/krlvm/powertunnel/BuildConstants.java
+++ core/src/main/java/io/github/krlvm/powertunnel/BuildConstants.java
@@ -20,9 +20,9 @@ package io.github.krlvm.powertunnel;
 public class BuildConstants {
     public static final String NAME = "PowerTunnel";
     public static final String REPO = "https://github.com/krlvm/PowerTunnel";
-    public static final String VERSION = "2.1";
-    public static final int VERSION_CODE = 102;
-    public static final int SDK = 102;
+    public static final String VERSION = "2.2";
+    public static final int VERSION_CODE = 103;
+    public static final int SDK = 103;
 
     public static boolean IS_RELEASE = isReleaseVersion(VERSION);
     public static final boolean DEBUG = !IS_RELEASE;
