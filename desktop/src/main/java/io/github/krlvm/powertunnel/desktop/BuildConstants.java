b06225495f82f3c91235020759336cb628c4356c
krlvm <51774833+krlvm@users.noreply.github.com>
2022-07-26 05:10:12 +0400

    Desktop: Bump version to 2.4 (107)

--- desktop/src/main/java/io/github/krlvm/powertunnel/desktop/BuildConstants.java
+++ desktop/src/main/java/io/github/krlvm/powertunnel/desktop/BuildConstants.java
@@ -21,8 +21,8 @@ public class BuildConstants {
     public static final String NAME = "PowerTunnel";
     public static final String DESCRIPTION = "Powerful and extensible proxy server";
     public static final String REPO = "https://github.com/krlvm/PowerTunnel";
-    public static final String VERSION = "2.3";
-    public static final int VERSION_CODE = 106;
+    public static final String VERSION = "2.4";
+    public static final int VERSION_CODE = 107;
 
     public static boolean IS_RELEASE = io.github.krlvm.powertunnel.BuildConstants.isReleaseVersion(VERSION);
     public static final boolean DEBUG = !IS_RELEASE;
