7ae84d24eea42c68b5ce75abd51fbbe46a439c51
krlvm <51774833+krlvm@users.noreply.github.com>
2022-07-31 17:35:44 +0400

    Desktop: Fixed an issue when configuration set via CLI was persisting

--- desktop/src/main/java/io/github/krlvm/powertunnel/desktop/configuration/AdvancedConfiguration.java
+++ desktop/src/main/java/io/github/krlvm/powertunnel/desktop/configuration/AdvancedConfiguration.java
@@ -19,48 +19,52 @@ package io.github.krlvm.powertunnel.desktop.configuration;
 
 import io.github.krlvm.powertunnel.configuration.ConfigurationStore;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class AdvancedConfiguration extends ConfigurationStore {
 
-    private final Collection<String> immutableKeys;
+    private final Collection<String> immutableKeys = new HashSet<>();
+    private final Map<String, String> preImmutableData = new HashMap<>();
 
-    public AdvancedConfiguration() {
-        this(new ArrayList<>());
-    }
-
-    public AdvancedConfiguration(Collection<String> immutableKeys) {
-        this.immutableKeys = immutableKeys;
-    }
-
-    public void protect(String key) {
-        this.immutableKeys.add(key);
+    private void saveOriginalKey(String key) {
+        String originalVal = get(key, null);
+        if (originalVal != null) {
+            preImmutableData.put(key, originalVal);
+        }
     }
 
     public void protect(String key, String value) {
         this.immutableKeys.add(key);
+        saveOriginalKey(key);
         set(key, value);
     }
 
     public void protectInt(String key, int value) {
         this.immutableKeys.add(key);
+        saveOriginalKey(key);
         setInt(key, value);
     }
 
     public void protectBoolean(String key, boolean value) {
         this.immutableKeys.add(key);
+        saveOriginalKey(key);
         setBoolean(key, value);
     }
 
     @Override
     protected Set<Map.Entry<String, String>> entries() {
-        return super.entries().stream()
-                .filter(entry -> !immutableKeys.contains(entry.getKey()))
-                .collect(Collectors.toSet());
+        Set<Map.Entry<String, String>> entries = new HashSet<>();
+
+        for (Map.Entry<String, String> entry : super.entries()) {
+            if (immutableKeys.contains(entry.getKey())) {
+                if (!preImmutableData.containsKey(entry.getKey())) continue;
+                entry = new AbstractMap.SimpleEntry<>(entry.getKey(), preImmutableData.get(entry.getKey()));
+            }
+            entries.add(entry);
+        }
+
+        return entries;
     }
 
     public Collection<String> getImmutableKeys() {
